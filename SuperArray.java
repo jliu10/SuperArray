cb98f5dc72554ba6b995174e9b4edd127a9a6e26
jliu10 <jliu10@stuy.edu>
2020-11-01 01:16:08 -0500

    modified constructor

--- SuperArray.java
+++ SuperArray.java
@@ -2,7 +2,7 @@ public class SuperArray{
   private String [] data;
   private int size; //The current size
   public SuperArray(){
-    data=new String[10]
+    data=new data[10]
     size=0;
   }
 }
