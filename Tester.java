8f4f763b04ddaf56119e0614c91ec80a9f358ef7
jliu10 <jliu10@stuy.edu>
2020-11-01 01:57:10 -0500

    tested methods size, add

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester{
+  public static void main(String[] args){
+    SuperArray words = new SuperArray();
+    System.out.println(words);
+    System.out.println(words.size());
+    System.out.println(words.add("first"));
+    System.out.println(words.size());
+  }
+}
