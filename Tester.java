c72ca2b47674abb355c98a46643e5560313fc19d
jliu10 <jliu10@stuy.edu>
2020-11-02 01:11:23 -0500

    tested method add, resize

--- Tester.java
+++ Tester.java
@@ -4,13 +4,36 @@ public class Tester{
     System.out.println(words);
     System.out.println(words.size());
 //    System.out.println(words.get(0));
-    System.out.println(words.add("first"));
+    System.out.println(words.add("jan"));
+    System.out.println(words.size());
+    System.out.println(words.add("feb"));
     System.out.println(words.size());
     System.out.println(words.get(0));
     System.out.println(words.get(1));
-    System.out.println(words.set(1,"pea"));
+    System.out.println(words.get(2));
     System.out.println(words.set(0,"pea"));
+    System.out.println(words.set(5,"pea"));
     System.out.println(words.size());
     System.out.println(words.get(0));
+    System.out.println(words.add("mar"));
+    System.out.println(words.size());
+    System.out.println(words.add("apr"));
+    System.out.println(words.size());
+    System.out.println(words.add("may"));
+    System.out.println(words.size());
+    System.out.println(words.add("jun"));
+    System.out.println(words.size());
+    System.out.println(words.add("jul"));
+    System.out.println(words.size());
+    System.out.println(words.add("aug"));
+    System.out.println(words.size());
+    System.out.println(words.add("sep"));
+    System.out.println(words.size());
+    System.out.println(words.add("oct"));
+    System.out.println(words.size());
+    System.out.println(words.add("nov"));
+    System.out.println(words.size());
+    System.out.println(words.add("dec"));
+    System.out.println(words.size());
   }
 }
