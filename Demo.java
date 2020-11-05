2d54e05bdd2eee82a12456ed7e4be1a051657794
jliu10 <jliu10@stuy.edu>
2020-11-05 15:06:16 -0500

    added method removeDuplicates, is WIP

--- Demo.java
+++ Demo.java
@@ -1,6 +1,12 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
-    
+    if(s.size()>1){
+      for(int i=0;i<s.size();i++){
+        for(int j=i+1;j<s.size();j++){
+          if(s.get(j).equals(s.get(i))) s.remove(j);
+        }
+      }
+    }
   }
   public static void main(String[]args){
     SuperArray words = new SuperArray();
