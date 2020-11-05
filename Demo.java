cc9ee21e7faa4a7d357b65ca74a1d184614166a4
jliu10 <jliu10@stuy.edu>
2020-11-05 15:36:22 -0500

    fixed method removeDuplicates

--- Demo.java
+++ Demo.java
@@ -1,9 +1,15 @@
 public class Demo{
   public static void removeDuplicates(SuperArray s){
+    //System.out.println("\nRemoving duplicates...");
     if(s.size()>1){
       for(int i=0;i<s.size();i++){
-        for(int j=i+1;j<s.size();j++){
+        //System.out.println(s);
+        //System.out.println("i is "+i);
+        for(int j=s.size()-1;j>i;j--){
+          //System.out.println("j is "+j);
           if(s.get(j).equals(s.get(i))) s.remove(j);
+          //System.out.println("size is "+s.size());
+          //System.out.println(s);
         }
       }
     }
@@ -14,8 +20,19 @@ public class Demo{
     words.add("kani");   words.add("uni");     words.add("ebi");     words.add("una");
     words.add("una");    words.add("ebi");     words.add("kani");    words.add("una");
     words.add("una");    words.add("ebi");     words.add("toro");
-
+/*
+    SuperArray words1 = new SuperArray();
+    words1.add("una"); words1.add("una"); words1.add("bruh"); words1.add("una");
+    words1.add("una"); words1.add("una"); words1.add("toro"); words1.add("toro");
+    words1.add("una"); words1.add("toro");
+*/
     System.out.println(words);
     removeDuplicates(words);
-    System.out.println(words);  }
+    System.out.println(words);
+/*
+    System.out.println(words1);
+    removeDuplicates(words1);
+    System.out.println(words1);
+*/
+  }
 }
